24a30756835e5777dd5b9548aaed7407c9370dc0
vadhin11 <vadhin11@gmail.com>
2025-12-26 12:00:27 +0700

    add text to page

--- 02_frontend/app/page.js
+++ 02_frontend/app/page.js
@@ -44,6 +44,7 @@ export default function Page() {
   return (
     <main className="container">
       <header className="header">
+        <h1 className="title">Vadhin Page</h1>
         <h1 className="title">Attractions</h1>
         <p className="subtitle">Discover points of interest nearby</p>
       </header>
