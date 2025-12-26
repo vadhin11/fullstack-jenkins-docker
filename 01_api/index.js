90a021bd47cace7cf956c9148e06ac02f605c2e0
vadhin11 <vadhin11@gmail.com>
2025-12-26 03:09:51 +0000

    change to default port 3306

--- 01_api/index.js
+++ 01_api/index.js
@@ -13,7 +13,7 @@ const pool = mysql.createPool({
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || 'password',
   database: process.env.DB_NAME || 'test',
-  port: Number(process.env.DB_PORT || 3307),
+  port: Number(process.env.DB_PORT || 3306),
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
