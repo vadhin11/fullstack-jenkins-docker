d50df3759afd7e37ab4a4c9e05513a6b9c7e4c90
vadhin11 <vadhin11@gmail.com>
2025-12-29 09:17:01 +0700

    remove header page

--- 02_frontend/app/page.js
+++ 02_frontend/app/page.js
@@ -44,7 +44,6 @@ export default function Page() {
   return (
     <main className="container">
       <header className="header">
-        <h1 className="title">Vadhin Page</h1>
         <h1 className="title">Attractions</h1>
         <p className="subtitle">Discover points of interest nearby</p>
       </header>
